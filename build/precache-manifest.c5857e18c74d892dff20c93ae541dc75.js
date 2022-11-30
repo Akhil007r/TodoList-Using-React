4462abea9dea5d4fecdf24eb4f88160ceabcccfb
Akhilesh R <akhilesh.r2000@gmail.com>
2022-11-30 08:54:58 +0000

    Create 12 files

---
+++ build/precache-manifest.c5857e18c74d892dff20c93ae541dc75.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206847c6731b6e5f7a9532d7d0d4165d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7da3f1b26b30b9ec3486",
+    "url": "/static/js/2.498712e0.chunk.js"
+  },
+  {
+    "revision": "d628643cabd6b40242a9",
+    "url": "/static/js/main.69c0a48e.chunk.js"
+  },
+  {
+    "revision": "553a30039c88ec7c9707",
+    "url": "/static/js/runtime-main.7ac9bc12.js"
+  }
+]);
